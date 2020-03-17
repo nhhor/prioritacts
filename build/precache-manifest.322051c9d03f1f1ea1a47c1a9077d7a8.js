e355fe149b17dcbc4b8b831f5edfb77b8ad11f09
nhhor <nhhor@post.com>
2020-03-16 17:40:19 -0700

    add build for netlify deployment.

---
+++ build/precache-manifest.322051c9d03f1f1ea1a47c1a9077d7a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf4aada95f0fa903f33cf9eccd9f6c42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bf771bdc8a8fc7809d3",
+    "url": "/static/css/main.793cc69e.chunk.css"
+  },
+  {
+    "revision": "cedbafda763eca4cf503",
+    "url": "/static/js/2.de217f47.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.de217f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf771bdc8a8fc7809d3",
+    "url": "/static/js/main.349cdfd2.chunk.js"
+  },
+  {
+    "revision": "e0ebdb4aafad9dc02f58",
+    "url": "/static/js/runtime-main.9b887025.js"
+  }
+]);
