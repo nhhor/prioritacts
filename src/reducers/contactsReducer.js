37ff79eb9246031f6adf13095ab5c6fe4109678e
nhhor <nhhor@post.com>
2020-05-20 16:47:17 -0700

    update sort for when freqA# ==  freqB#.

--- src/reducers/contactsReducer.js
+++ src/reducers/contactsReducer.js
@@ -22,47 +22,41 @@ export default (state = initialState.contacts, action) => {
       // return contactA - contactB;
     // }
 
-
-
     // REF: https://stackoverflow.com/questions/4340227/sort-mixed-alpha-numeric-array
     var reA = /[^a-zA-Z]/g;
     var reN = /[^0-9]/g;
     function sortAlphaNum(a,b) {
-        const contactA = (a.userDefined && a.userDefined.find(x => x.key === '~prioritacts~frequency~') !== undefined) ? a.userDefined.find(x => x.key === '~prioritacts~frequency~').value : a.names[0].displayName.toUpperCase();
-        const contactB = (b.userDefined && b.userDefined.find(x => x.key === '~prioritacts~frequency~') !== undefined) ? b.userDefined.find(x => x.key === '~prioritacts~frequency~').value : b.names[0].displayName.toUpperCase();
-        var AInt = parseInt(contactA, 10);
-        var BInt = parseInt(contactB, 10);
-        if(isNaN(AInt) && isNaN(BInt)){
-            var aA = contactA.replace(reA, "");
-            var bA = contactB.replace(reA, "");
-            if(aA === bA) {
-                var aN = parseInt(contactA.replace(reN, ""), 10);
-                var bN = parseInt(contactB.replace(reN, ""), 10);
-                return aN === bN ? 0 : aN > bN ? 1 : -1;
-            } else {
-                return aA > bA ? 1 : -1;
-            }
-        }else if(isNaN(AInt)){//A is not an In
-            return 1;//to make alphanumeric sort first return -1 here
-        }else if(isNaN(BInt)){//B is not an In
-            return -1;//to make alphanumeric sort first return 1 here
-        }else{
-            return AInt > BInt ? 1 : -1;
+      const contactA = (a.userDefined && a.userDefined.find(x => x.key === '~prioritacts~frequency~') !== undefined) ? a.userDefined.find(x => x.key === '~prioritacts~frequency~').value : a.names[0].displayName.toUpperCase();
+      const contactB = (b.userDefined && b.userDefined.find(x => x.key === '~prioritacts~frequency~') !== undefined) ? b.userDefined.find(x => x.key === '~prioritacts~frequency~').value : b.names[0].displayName.toUpperCase();
+      var AInt = parseInt(contactA, 10);
+      var BInt = parseInt(contactB, 10);
+      if(isNaN(AInt) && isNaN(BInt)){
+        var aA = contactA.replace(reA, "");
+        var bA = contactB.replace(reA, "");
+        if(aA === bA) {
+          var aN = parseInt(contactA.replace(reN, ""), 10);
+          var bN = parseInt(contactB.replace(reN, ""), 10);
+          return aN === bN ? 0 : aN > bN ? 1 : -1;
+        } else {
+          return aA > bA ? 1 : -1;
         }
+      }else if(isNaN(AInt)){//A is not an In
+        return 1;//to make alphanumeric sort first return -1 here
+      }else if(isNaN(BInt)){//B is not an In
+        return -1;//to make alphanumeric sort first return 1 here
+      } else if (AInt == BInt) {
+        var aA2 = contactA.replace(reA, "");
+        var bA2 = contactB.replace(reA, "");
+        return aA2 > bA2 ? 1 : -1;
+      } else{
+        return AInt > BInt ? 1 : -1;
+      }
     }
     let sortedPayload = toSort.sort(sortAlphaNum);
-
-
-
     return {
       contacts: sortedPayload
     }
     case types.LOG_EVENT:
-
-
-
-
-
     let logUpdatedState = state.contacts.map(contactToLogUpdate => {
       if (contactToLogUpdate.resourceName === action.payload.resourceName) {
         return action.payload;
@@ -70,42 +64,37 @@ export default (state = initialState.contacts, action) => {
         return contactToLogUpdate;
       }
     });
-
-
     // REF: https://stackoverflow.com/questions/4340227/sort-mixed-alpha-numeric-array
     var reA2 = /[^a-zA-Z]/g;
     var reN2 = /[^0-9]/g;
     function sortAlphaNum2(a,b) {
-        const contactA = (a.userDefined && a.userDefined.find(x => x.key === '~prioritacts~frequency~') !== undefined) ? a.userDefined.find(x => x.key === '~prioritacts~frequency~').value : a.names[0].displayName.toUpperCase();
-        const contactB = (b.userDefined && b.userDefined.find(x => x.key === '~prioritacts~frequency~') !== undefined) ? b.userDefined.find(x => x.key === '~prioritacts~frequency~').value : b.names[0].displayName.toUpperCase();
-        var AInt = parseInt(contactA, 10);
-        var BInt = parseInt(contactB, 10);
-        if(isNaN(AInt) && isNaN(BInt)){
-            var aA = contactA.replace(reA2, "");
-            var bA = contactB.replace(reA2, "");
-            if(aA === bA) {
-                var aN = parseInt(contactA.replace(reN2, ""), 10);
-                var bN = parseInt(contactB.replace(reN2, ""), 10);
-                return aN === bN ? 0 : aN > bN ? 1 : -1;
-            } else {
-                return aA > bA ? 1 : -1;
-            }
-        }else if(isNaN(AInt)){//A is not an In
-            return 1;//to make alphanumeric sort first return -1 here
-        }else if(isNaN(BInt)){//B is not an In
-            return -1;//to make alphanumeric sort first return 1 here
-        }else{
-            return AInt > BInt ? 1 : -1;
+      const contactA = (a.userDefined && a.userDefined.find(x => x.key === '~prioritacts~frequency~') !== undefined) ? a.userDefined.find(x => x.key === '~prioritacts~frequency~').value : a.names[0].displayName.toUpperCase();
+      const contactB = (b.userDefined && b.userDefined.find(x => x.key === '~prioritacts~frequency~') !== undefined) ? b.userDefined.find(x => x.key === '~prioritacts~frequency~').value : b.names[0].displayName.toUpperCase();
+      var AInt = parseInt(contactA, 10);
+      var BInt = parseInt(contactB, 10);
+      if(isNaN(AInt) && isNaN(BInt)){
+        var aA = contactA.replace(reA2, "");
+        var bA = contactB.replace(reA2, "");
+        if(aA === bA) {
+          var aN = parseInt(contactA.replace(reN2, ""), 10);
+          var bN = parseInt(contactB.replace(reN2, ""), 10);
+          return aN === bN ? 0 : aN > bN ? 1 : -1;
+        } else {
+          return aA > bA ? 1 : -1;
         }
+      }else if(isNaN(AInt)){//A is not an In
+        return 1;//to make alphanumeric sort first return -1 here
+      }else if(isNaN(BInt)){//B is not an In
+        return -1;//to make alphanumeric sort first return 1 here
+      } else if (AInt == BInt) {
+        var aA2 = contactA.replace(reA, "");
+        var bA2 = contactB.replace(reA, "");
+        return aA2 > bA2 ? 1 : -1;
+      } else {
+        return AInt > BInt ? 1 : -1;
+      }
     }
     let sortedPayload2 = logUpdatedState.sort(sortAlphaNum2);
-
-
-
-
-
-
-
     return {
       contacts: sortedPayload2,
     }
